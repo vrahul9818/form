eaa477cecef28a0b1e77751dddf241cf149c407c
vrahul9818 <122368342+vrahul9818@users.noreply.github.com>
2024-10-03 15:37:58 +0530

    Update BookingHistoryStep.js for backend deployed link

--- frontend/src/components/BookingHistoryStep.js
+++ frontend/src/components/BookingHistoryStep.js
@@ -14,7 +14,7 @@ const BookingHistoryStep = ({ prevStep, formData }) => {
     const handleBooking = async () => {
       setSubmitting(true);
       try {
-        const response = await fetch('http://localhost:5000/api/bookings/add-booking', {
+        const response = await fetch('https://form-backend-ybco.onrender.com/api/bookings/add-booking', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
